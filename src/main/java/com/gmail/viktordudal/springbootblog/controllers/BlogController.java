0f209a917b111f74ad27602c8b983715a0c6b79c
Viktor Dudal <viktordudal@gmail.com>
2023-02-09 14:19:17 +0200

    Added post details page

--- src/main/java/com/gmail/viktordudal/springbootblog/controllers/BlogController.java
+++ src/main/java/com/gmail/viktordudal/springbootblog/controllers/BlogController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -35,6 +40,21 @@ public class BlogController {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable("id") long id, Model model) {
+
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        List<Post> result = new ArrayList<>();
+
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-details";
     }
 
 }
